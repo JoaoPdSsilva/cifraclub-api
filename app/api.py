79fd5cee2b31361d8bebeb13be256b7518d54ca8
João Pedro <60629168+JoaoPdSsilva@users.noreply.github.com>
2026-01-03 17:25:52 -0300

    Refactor API routes to use jsonify

--- app/api.py
+++ app/api.py
@@ -1,29 +1,32 @@
 """API Module"""
 
 import os
-from flask import Flask, json
+from flask import Flask, jsonify
 from cifraclub import CifraClub
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
     """Home route"""
-    return app.response_class(
-        response=json.dumps({'api': 'Cifra Club API'}),
-        status=200,
-        mimetype='application/json'
-    )
+    return jsonify({
+        "api": "Cifra Club API",
+        "status": "online"
+    })
 
-@app.route('/artists/<artist>/songs/<song>')
+@app.route("/song/<artist>/<song>")
 def get_cifra(artist, song):
     """Get cifra by artist and song"""
-    cifrablub = CifraClub()
-    return app.response_class(
-        response=json.dumps(cifrablub.cifra(artist, song), ensure_ascii=False),
-        status=200,
-        mimetype='application/json'
-    )
+
+    cifra_client = CifraClub()
+    result = cifra_client.cifra(artist, song)
+
+    if "error" in result:
+        return jsonify(result), 404
+
+    return jsonify(result), 200
+
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=os.getenv('PORT', '3000'), debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
